8ccbab7b5fa3e679e3a9cdc06c67005b4b137b3e
Bossant77 <Santiagoboliocano@gmail.com>
2026-01-20 09:44:33 -0600

    Add files via upload

---
+++ input output.py
@@ -0,0 +1,10 @@
+# Santiago de Jesús Bolio Cano
+#540809
+
+
+print("¿cual es tu nombre?") 
+nombre= str(input("nombre: "))
+print ("hola ", nombre, " escribe dos valores enteros: ")
+n1= int(input())
+n2= int(input())
+print("la suma es: ",  (n1+n2))
